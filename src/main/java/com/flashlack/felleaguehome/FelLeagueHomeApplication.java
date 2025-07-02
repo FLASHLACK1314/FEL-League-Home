5c0639b4da9775c027b53c3e227d852e25a9cbdb
FLASHLACK <flashlack1314@163.com>
2025-07-03 02:50:13 +0800

    chore: 更新 .gitignore并添加项目入口类注释- 在 .gitignore 中添加 .github/copilot-instructions.md 文件- 在 FelLeagueHomeApplication 类上添加项目入口点注释

--- src/main/java/com/flashlack/felleaguehome/FelLeagueHomeApplication.java
+++ src/main/java/com/flashlack/felleaguehome/FelLeagueHomeApplication.java
@@ -3,6 +3,10 @@ package com.flashlack.felleaguehome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * * SpringBoot应用程序FelLeagueHome的入口点。
+ * @author FLASHLACK
+ */
 @SpringBootApplication
 public class FelLeagueHomeApplication {
 
