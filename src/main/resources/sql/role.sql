1c20f26715553b1336b02191954a6effeab5afd2
FLASHLACK <flashlack1314@163.com>
2025-07-05 08:40:25 +0800

    feat(database): 创建用户表并调整角色表
    - 在数据库中创建用户表，包括用户唯一标识、用户名、密码、邮箱、QQ账号和角色标识字段
    - 为用户表和字段添加注释，便于理解表结构
    - 在用户表中添加外键约束，关联角色表
    - 调整角色表名称，使用双引号括起，以适应某些数据库的保留字问题

--- src/main/resources/sql/role.sql
+++ src/main/resources/sql/role.sql
@@ -1,4 +1,4 @@
-create table role
+create table "role"
 (
     role_uuid   varchar(36) not null
         primary key,
---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,23 @@
+create table "user"
+(
+    user_uuid  varchar(36)  not null
+        primary key,
+    username   varchar(50)  not null
+        unique,
+    password   varchar(100) not null,
+    email      varchar(100) not null
+        unique,
+    qq_account varchar(20),
+    role_uuid  varchar(36)  not null
+);
+
+comment on table "user" is '用户表';
+comment on column "user".user_uuid is '用户唯一标识';
+comment on column "user".username is '用户名';
+comment on column "user".password is '用户密码';
+comment on column "user".email is '用户邮箱';
+comment on column "user".qq_account is '用户QQ账号';
+comment on column "user".role_uuid is '用户角色标识';
+
+ALTER TABLE "user"
+    ADD CONSTRAINT fk_user_role FOREIGN KEY (role_uuid) REFERENCES "role" (role_uuid);
